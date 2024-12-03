6bb57b961596fda46e16b44dc9eb8b6364ad5a93
Johnhyde Policharla <100892286+hyde124john@users.noreply.github.com>
2024-12-03 12:42:01 +0530

    Create AddSpaces.java

---
+++ AddSpaces.java
@@ -0,0 +1,15 @@
+class AddSpaces {
+    public String addSpaces(String s, int[] spaces) {
+        StringBuilder res = new StringBuilder();
+        int s1 = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s1 < spaces.length && spaces[s1] == i) {
+                res.append(" "); 
+                s1++; 
+            }
+            res.append(s.charAt(i));
+        }
+
+        return res.toString();
+    }
+}
